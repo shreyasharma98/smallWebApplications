b25d8992c877ceb278d4e5000dd1a126bc166c99
shreysharma98 <shreya.36.ss@gmail.com>
2021-04-25 14:19:21 +0530

    update timer bug

--- liveTimerApp/app.js
+++ liveTimerApp/app.js
@@ -1,9 +1,10 @@
 let x;
 let fullDate;
-let days = ["Monday","Tuesday","Wednesday","Thursday","friday","Saturday","Sunday"];
+let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","friday","Saturday"];
 setInterval(() => {
     x = new Date();
     let day = x.getDay();
-    fullDate = x.getHours() + ':' +  x.getMinutes() + ':' + x.getSeconds()+ " <br> on   "+days[day-1]+" , " + x.getDate() + '/' + x.getMonth() + '/' + x.getFullYear();
+    console.log(day);
+    fullDate = x.getHours() + ':' +  x.getMinutes() + ':' + x.getSeconds()+ " <br> on   "+days[day]+" , " + x.getDate() + '/' + x.getMonth() + '/' + x.getFullYear();
     document.getElementById("currentTime").innerHTML = fullDate;
 }, 1000);
